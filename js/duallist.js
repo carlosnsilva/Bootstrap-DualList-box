d4682d75e9adc0d3dc0264209e1cc232a3173d6a
carlosnsilva <cmanoel17@gmail.com>
2020-01-09 08:15:08 -0300

    First Commit

---
+++ js/duallist.js
@@ -0,0 +1,6 @@
+var demo1 = $('select[name="duallistbox_demo1"]').bootstrapDualListbox({
+    nonSelectedListLabel: 'Non-selected',
+    selectedListLabel: 'Selected',
+    preserveSelectionOnMove: 'moved',
+    moveOnSelect: false,
+});
